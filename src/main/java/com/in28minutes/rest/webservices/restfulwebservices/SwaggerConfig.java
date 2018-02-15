d783c174db4a98ce9ab6977a776f06f8675b5cf2
Franck Mukendi <franck.mukendi@gmail.com>
2018-02-14 21:43:16 -0500

    completed swager

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,22 +1,56 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.assertj.core.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //Configuration
 @Configuration
-//Enable Swagger
+// Enable Swagger
 @EnableSwagger2
 public class SwaggerConfig {
+
+	public static final Contact DEFAULT_CONTACT = new Contact(
+			"Franck Mukendi", "htt://www.mukeapps.com", "franck.mukendi@gmail.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+			"Awsome API Title", "Awsome API Description", "1.0", "urn:tos",
+			DEFAULT_CONTACT, "Apache 2.0", 
+			"http://www.apache.org/licenses/LICENSE-2.0");
+	
+/*	private static final  Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
+			new HashSet<String>(Arrays.asList("application/json","application/xml"));// prefered method */
+	private Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
+			new HashSet<String>();
+
+	// @Bean
+	// public Docket api() {
+	// return new Docket(DocumentationType.SWAGGER_2);
+	// }
+	//
+	/*@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
+	}*/
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		DEFAULT_PRODUCES_AND_CONSUMES.add("application/json");
+		DEFAULT_PRODUCES_AND_CONSUMES.add("application/xml");
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
-   //Bean - Docket
+	// Bean - Docket
 	// Swagger 2
 	// All the paths
 	// All the apis
